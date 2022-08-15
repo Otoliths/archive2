4bb5f944c11f49ce31bf5c482171b21d40b06bba
Liuyong Ding <33018532+Otoliths@users.noreply.github.com>
2022-08-15 15:46:57 +0800

    Update get_data.R

--- get_data.R
+++ get_data.R
@@ -5,14 +5,14 @@ install.packages('curl')
 
 path1 <- paste0("water/",as.POSIXlt(Sys.time(), "Asia/Shanghai"),".rds")
 
-water <- jsonlite::fromJSON("http://www.ynswj.cn/webapi/api/v1/water",fill_na = TRUE)
+water <- jsonlite::fromJSON("http://www.ynswj.cn/webapi/api/v1/water?extra=area&itm=1&area_code=530000&no_data_visible=false",fill_na = TRUE)
 
 saveRDS(water,path1)
 
 
 path2 <- paste0("rain/",as.POSIXlt(Sys.time(), "Asia/Shanghai"),".rds")
 
-rain <- jsonlite::fromJSON("http://www.ynswj.cn/webapi/api/v1/rain",fill_na = TRUE)
+rain <- jsonlite::fromJSON("http://www.ynswj.cn/webapi/api/v1/rain?extra=area&itm=1&area_code=530000&no_data_visible=false",fill_na = TRUE)
 
 saveRDS(rain,path2)
 
