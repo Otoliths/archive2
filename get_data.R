c53d6ff697aede160c560cc540e1a7eb4ab07155
Liuyong Ding <33018532+Otoliths@users.noreply.github.com>
2022-08-15 14:42:00 +0800

    Update get_data.R

--- get_data.R
+++ get_data.R
@@ -1,17 +1,29 @@
 options(timeout=1000)
 
-install.packages("jsonlite")
-install.packages('curl')
+# install.packages("jsonlite")
+# install.packages('curl')
+# 
+# path1 <- paste0("water/",as.POSIXlt(Sys.time(), "Asia/Shanghai"),".rds")
+# 
+# water <- jsonlite::fromJSON("http://www.ynswj.cn/webapi/api/v1/water",fill_na = TRUE)
+# 
+# saveRDS(water,path1)
+# 
+# 
+# path2 <- paste0("rain/",as.POSIXlt(Sys.time(), "Asia/Shanghai"),".rds")
+# 
+# rain <- jsonlite::fromJSON("http://www.ynswj.cn/webapi/api/v1/rain",fill_na = TRUE)
+# 
+# saveRDS(rain,path2)
+
+install.packages('httr')
+
+library(httr)
 
 path1 <- paste0("water/",as.POSIXlt(Sys.time(), "Asia/Shanghai"),".rds")
-
-water <- jsonlite::fromJSON("http://www.ynswj.cn/webapi/api/v1/water",fill_na = TRUE)
-
+water <- content(GET("http://www.ynswj.cn/webapi/api/v1/water"),as="parsed") 
 saveRDS(water,path1)
 
-
 path2 <- paste0("rain/",as.POSIXlt(Sys.time(), "Asia/Shanghai"),".rds")
-
-rain <- jsonlite::fromJSON("http://www.ynswj.cn/webapi/api/v1/rain",fill_na = TRUE)
-
+rain <- content(GET("http://www.ynswj.cn/webapi/api/v1/rain"),as="parsed") 
 saveRDS(rain,path2)
