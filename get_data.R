704cac58d186fdbf3c5cf8315076747402df7e33
Liuyong Ding <33018532+Otoliths@users.noreply.github.com>
2022-08-15 15:24:11 +0800

    Update get_data.R

--- get_data.R
+++ get_data.R
@@ -16,13 +16,37 @@ options(timeout=1000)
 # 
 # saveRDS(rain,path2)
 
+# install.packages('httr')
+# 
+# library(httr)
+# 
+# path1 <- paste0("water/",as.POSIXlt(Sys.time(), "Asia/Shanghai"),".rds")
+# water <- content(GET("http://www.ynswj.cn/webapi/api/v1/water?extra=area&itm=1"),as="parsed") 
+# saveRDS(water,path1)
+# 
+# path2 <- paste0("rain/",as.POSIXlt(Sys.time(), "Asia/Shanghai"),".rds")
+# rain <- content(GET("http://www.ynswj.cn/webapi/api/v1/rain"),as="parsed") 
+# saveRDS(rain,path2)
 
-library(httr)
 
+
+library(httr2)
+
+req1 <- request("http://www.ynswj.cn/webapi/api/v1/water?")
+
+resp1 <- req1 %>% 
+  req_url_query(`extra` = "area", `itm` = 1) %>% 
+  req_perform()
+water <- resp1 %>% resp_body_json()
 path1 <- paste0("water/",as.POSIXlt(Sys.time(), "Asia/Shanghai"),".rds")
-water <- content(GET("http://www.ynswj.cn/webapi/api/v1/water?extra=area&itm=1"),as="parsed") 
 saveRDS(water,path1)
 
+
+req2 <- request("http://www.ynswj.cn/webapi/api/v1/rain?")
+
+resp2 <- req2 %>% 
+  req_url_query(`extra` = "area", `itm` = 1) %>% 
+  req_perform()
+rain <- resp2 %>% resp_body_json()
 path2 <- paste0("rain/",as.POSIXlt(Sys.time(), "Asia/Shanghai"),".rds")
-rain <- content(GET("http://www.ynswj.cn/webapi/api/v1/rain"),as="parsed") 
 saveRDS(rain,path2)
