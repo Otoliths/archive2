f0cee4218f8dbbeb6004bd4c681bb90ab058b119
Liuyong Ding <33018532+Otoliths@users.noreply.github.com>
2022-08-15 09:49:50 +0800

    Add files via upload

---
+++ get_data.R
@@ -0,0 +1,12 @@
+path1 <- paste0("water/",as.POSIXlt(Sys.time(), "Asia/Shanghai"),".rds")
+
+water <- jsonify::from_json("http://www.ynswj.cn/webapi/api/v1/water",fill_na = TRUE)
+
+saveRDS(water,path1)
+
+
+path2 <- paste0("rain/",as.POSIXlt(Sys.time(), "Asia/Shanghai"),".rds")
+
+rain <- jsonify::from_json("http://www.ynswj.cn/webapi/api/v1/rain",fill_na = TRUE)
+
+saveRDS(rain,path2)
